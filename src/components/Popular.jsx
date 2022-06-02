f83704881493e2609fa361fd4d8a991e3cd292e9
David <dfan1315@gmail.com>
2022-06-02 14:19:05 -0700

    packages

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState} from "react";
+import styled from "styled-components";
 
 function Popular() {
     
@@ -18,7 +19,7 @@ function Popular() {
         <div>
             {popular.map((recipe) => {
             return (
-                <div>
+                <div key={recipe.id}>
                     <p>{recipe.title}</p>
                 </div>
                 );
